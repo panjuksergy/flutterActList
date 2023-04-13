11f01c8c9a9aaef89294333dc7a27c9a72ce09fe
Serhii <panjuksergy@gmail.com>
2023-04-13 13:51:53 +0300

    Add: svg Picture fore each Act on the ActListTile Fixed: bug with pictures on the ActListTile

--- lib/features/actionList/models/modelListFill.dart
+++ lib/features/actionList/models/modelListFill.dart
@@ -6,35 +6,35 @@ class ActionRepository {
         actionName: "Default calculator",
         actionDescription: "Default features like +,-,/,*",
         routeStr: "/default",
-        picRoute: 'assets/svg/1.svg'),
+        picRoute: 'assets/svg/calcSign.svg'),
     const Action(
         actionName: "Summation",
         actionDescription: "The summation of an explicit sequence",
         routeStr: "/summ",
-        picRoute: 'assets/svg/1.svg'),
+        picRoute: 'assets/svg/sumSign.svg'),
     const Action(
         actionName: "Factorial",
         actionDescription:
             "Factorial is the product of all positive integers less than or equal to n",
         routeStr: "/factorial",
-        picRoute: 'assets/svg/1.svg'),
+        picRoute: 'assets/svg/factSign.svg'),
     const Action(
         actionName: "Matrix addition",
         actionDescription:
             "Addition matrix with equal sum of rows and columns?",
         routeStr: "/matrix_addition",
-        picRoute: 'assets/svg/1.svg'),
+        picRoute: 'assets/svg/plusSign.svg'),
     const Action(
         actionName: "Matrix subtraction",
         actionDescription:
             "Subtraction matrix with equal sum of rows and columns?",
         routeStr: "/matrix_substraction",
-        picRoute: 'assets/svg/1.svg'),
+        picRoute: 'assets/svg/minusSign.svg'),
     const Action(
         actionName: "Matrix scalar multiplication",
         actionDescription:
             "Multiplying a matrix a by a scalar k is done by multiplying each element of the matrix by k.",
         routeStr: "/matrix_scalar_multiply",
-        picRoute: 'assets/svg/1.svg'),
+        picRoute: 'assets/svg/scalarSign.svg'),
   ];
 }
